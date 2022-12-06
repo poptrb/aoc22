20523e16919acd07851725d2315f42979a49e160
Iulian Popescu <popescuiulian67@gmail.com>
2022-12-06 12:49:18 +0200

    Formatting.

--- 01/solution.py
+++ 01/solution.py
@@ -3,8 +3,10 @@
 with open('input.txt', 'r') as file:
   data = ''.join(file.readlines()).split('\n\n')
 
-sol1 = (max([sum([ int(y) for y in x.split(' ') if y ]) for x in map(lambda x: x.replace('\n', ' '),data)]))
+sol1 = (max([sum(
+    [ int(y) for y in x.split(' ') if y ]) for x in map(lambda x: x.replace('\n', ' '),data)]))
 
-sol2 = sum(sorted([sum([ int(y) for y in x.split(' ') if y ]) for x in map(lambda x: x.replace('\n', ' '),data)])[-3:])
+sol2 = sum(sorted(
+    [sum([ int(y) for y in x.split(' ') if y ]) for x in map(lambda x: x.replace('\n', ' '),data)])[-3:])
 
 print(f"Solution 1: {sol1}\nSolution 2: {sol2}")
--- 02/solution.py
+++ 02/solution.py
@@ -1,11 +1,24 @@
 #!/usr/bin/env python
 
+# 1.
 # A, X = rock; B, Y = paper; C, Z = scissors
+# -1 = loss, 0 = draw, 1 = win
+
 #    | A   B   C
 #    |----------
-#  X | 0   1   -1
-#  Y | 1  0   1
-#  Z | -1   -1  0
+#  X | 0   1  -1
+#  Y | 1   0   1
+#  Z |-1  -1   0
+
+# 2.
+# X = loss, Y = draw, Z = win
+# -1 = rock, 0 = paper, 1 = scrissors
+
+#    | A   B   C
+#    |----------
+#  X | 1  -1   0
+#  Y |-1   0   1
+#  Z | 0   1  -1
 
 
 def convert(x):
@@ -15,8 +28,12 @@ def convert(x):
 with open('input.txt', 'r') as file:
   data = [ x.strip('\n').split(' ') for x in file.readlines() ]
 
-# Y is my choice, X is opponent's
 outcome = [[0, -1, 1], [1, 0, -1], [-1, 1, 0]]
-results = [ convert(y) + 1 + 3 * ( - outcome[convert(x)][convert(y)] + 1)  for x, y in data]
+my_play = [[1, -1, 0], [-1, 0, 1], [0, 1, -1]]
 
-print(sum(results))
+# y is my choice, x is opponent's
+sol1 = sum([ convert(y) + 1 + 3 * ( - outcome[convert(x)][convert(y)] + 1)  for x, y in data])
+# y is outcome, x is opponent's choice
+sol2 = sum([ convert(y) * 3 + my_play[convert(x)][convert(y)] + 2 for x,y in data ])
+
+print(f"Solution 1: {sol1}\nSolution 2: {sol2}")
